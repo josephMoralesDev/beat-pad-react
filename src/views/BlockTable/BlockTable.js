bc329025a7c70bfee28b2cb2af07c0bfcc11a1c8
Joseph <jmorales@movableink.com>
2019-01-20 21:36:46 -0500

    refactor some stuff

--- src/views/BlockTable/BlockTable.js
+++ src/views/BlockTable/BlockTable.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Block from '../../components/Block/Block';
 import './BlockTable.css';
-class BlockTable extends Component {
 
-  render() {
-    return (
-      <div className="grid-container">
-        {this.props.pads.map((pad, i) =>
-          <Block
-            key={i}
-            clipIndex={i}
-            audioId={`${pad.name}-${i}`}
-            audioSource={pad.sound}
-            padName={pad.name}
-          />
-        )}
+const BlockTable = (props) => {
+  return (
+    <div
+      className="grid-container"
+    >
+      {props.pads.map((pad, i) =>
+        <Block
+          key={i}
+          clipIndex={i}
+          audioId={`${pad.name}-${i}`}
+          audioSource={pad.sound}
+          padName={pad.name}
+        />
+      )}
 
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default BlockTable;
